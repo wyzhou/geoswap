c0517d18c83ceb929895ccf4e66de1ff0aab41bb
Wenyi <wyzhou_98@yahoo.com>
2010-09-11 16:27:01 -1000

    force change

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,7 @@ class LocationsController < ApplicationController
   def near_by
     respond_to do |format|
       format.html {}
-      format.json { render :json => Location.within(params[:lat], params[:lng], 0.2) }
+      format.json { render :json => Location.within(params[:lat], params[:lng], 5) }
     end
   end
 
